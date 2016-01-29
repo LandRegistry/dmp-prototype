3e1794e171cb5e2b48b1d43f2ab49304b90e9fdc
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-01-29 15:17:05 +0000

    routes and pages

--- app/routes.js
+++ app/routes.js
@@ -11,6 +11,25 @@ router.get('/examples/template-data', function (req, res) {
   res.render('examples/template-data', { 'name' : 'Foo' });
 });
 
-// add your routes here
+router.get('/:version/applications', function (req, res) {
+  var version =  req.params.version;
+  res.render(version + '/applications');
+});
+
+router.get('/:version/applications/:application', function (req, res) {
+  var version =  req.params.version;
+  res.render(version + '/application', { 'app' : req.params.application });
+});
+
+router.get('/:version/applications/:application/draft', function (req, res) {
+  var version =  req.params.version;
+  res.render(version + '/draft_register', { 'app' : req.params.application });
+});
+
+router.get('/:version/applications/:application/confirm', function (req, res) {
+  var version =  req.params.version;
+  res.render(version + '/confirm', { 'app' : req.params.application });
+});
+
 
 module.exports = router;
