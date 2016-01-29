a7f147e3d382d6fbc22b3d7db6a0a611cd4c44c5
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-01-29 16:45:53 +0000

    pass version, set page titles

--- app/routes.js
+++ app/routes.js
@@ -5,31 +5,24 @@ router.get('/', function (req, res) {
   res.render('index');
 });
 
-
-// Example route: Passing data into a page
-router.get('/examples/template-data', function (req, res) {
-  res.render('examples/template-data', { 'name' : 'Foo' });
-});
-
 router.get('/:version/applications', function (req, res) {
   var version =  req.params.version;
-  res.render(version + '/applications');
+  res.render(version + '/applications', { 'ver' : req.params.version });
 });
 
 router.get('/:version/applications/:application', function (req, res) {
   var version =  req.params.version;
-  res.render(version + '/application', { 'app' : req.params.application });
+  res.render(version + '/application', { 'app' : req.params.application, 'ver' : req.params.version });
 });
 
 router.get('/:version/applications/:application/draft', function (req, res) {
   var version =  req.params.version;
-  res.render(version + '/draft_register', { 'app' : req.params.application });
+  res.render(version + '/draft_register', { 'app' : req.params.application, 'ver' : req.params.version });
 });
 
 router.get('/:version/applications/:application/confirm', function (req, res) {
   var version =  req.params.version;
-  res.render(version + '/confirm', { 'app' : req.params.application });
+  res.render(version + '/confirm', { 'app' : req.params.application, 'ver' : req.params.version });
 });
-
 
 module.exports = router;
