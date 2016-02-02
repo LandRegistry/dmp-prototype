9db5ee1795852a4e333062c040f01ed287ffe24c
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2016-02-02 16:49:01 +0000

    service name, dashboard route

--- app/routes.js
+++ app/routes.js
@@ -5,24 +5,29 @@ router.get('/', function (req, res) {
   res.render('index');
 });
 
+router.get('/:version/dashboard', function (req, res) {
+  var version =  req.params.version;
+  res.render(version + '/dashboard', { 'ver' : version});
+});
+
 router.get('/:version/applications', function (req, res) {
   var version =  req.params.version;
-  res.render(version + '/applications', { 'ver' : req.params.version });
+  res.render(version + '/applications', { 'ver' : version});
 });
 
 router.get('/:version/applications/:application', function (req, res) {
   var version =  req.params.version;
-  res.render(version + '/application', { 'app' : req.params.application, 'ver' : req.params.version });
+  res.render(version + '/application', { 'app' : req.params.application, 'ver' : version });
 });
 
 router.get('/:version/applications/:application/draft', function (req, res) {
   var version =  req.params.version;
-  res.render(version + '/draft_register', { 'app' : req.params.application, 'ver' : req.params.version });
+  res.render(version + '/draft_register', { 'app' : req.params.application, 'ver' : version });
 });
 
 router.get('/:version/applications/:application/confirm', function (req, res) {
   var version =  req.params.version;
-  res.render(version + '/confirm', { 'app' : req.params.application, 'ver' : req.params.version });
+  res.render(version + '/confirm', { 'app' : req.params.application, 'ver' : version });
 });
 
 module.exports = router;
